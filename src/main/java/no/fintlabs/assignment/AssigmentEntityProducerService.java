42fd47e204ab7664e3bf313b4f8f5ceaaad1675a
Bernt Andreas Johnsen <berntj@viken.no>
2023-05-30 15:13:54 +0200

    Add kafka producer for resource group memberships

---
+++ src/main/java/no/fintlabs/assignment/AssigmentEntityProducerService.java
@@ -0,0 +1,32 @@
+package no.fintlabs.assignment;
+
+import no.fintlabs.kafka.entity.EntityProducer;
+import no.fintlabs.kafka.entity.EntityProducerFactory;
+import no.fintlabs.kafka.entity.EntityProducerRecord;
+import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssigmentEntityProducerService {
+    private final EntityProducer<SimpleAssignment> entityProducer;
+    private final EntityTopicNameParameters entityTopicNameParameters;
+    public AssigmentEntityProducerService( EntityProducerFactory entityProducerFactory)
+    {
+        entityProducer = entityProducerFactory.createProducer(SimpleAssignment.class);
+        entityTopicNameParameters = EntityTopicNameParameters
+                .builder()
+                .resource("resourcegroupmembership")
+                .build();
+    }
+
+    public void publish(Assignment assignment) {
+        String key = assignment.getAssignmentId();
+        entityProducer.send(
+                EntityProducerRecord.<SimpleAssignment>builder()
+                        .topicNameParameters(entityTopicNameParameters)
+                        .key(key)
+                        .value(assignment.toSimpleAssignment())
+                        .build()
+        );
+    }
+}
