5a4f66d333aadcc1cd0e936073a325e8ac261fb8
Bernt Andreas Johnsen <berntj@viken.no>
2023-06-30 13:57:25 +0200

    add ensureTopic to entityProducerService

--- src/main/java/no/fintlabs/assignment/AssigmentEntityProducerService.java
+++ src/main/java/no/fintlabs/assignment/AssigmentEntityProducerService.java
@@ -4,19 +4,24 @@ import no.fintlabs.kafka.entity.EntityProducer;
 import no.fintlabs.kafka.entity.EntityProducerFactory;
 import no.fintlabs.kafka.entity.EntityProducerRecord;
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import no.fintlabs.kafka.entity.topic.EntityTopicService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AssigmentEntityProducerService {
     private final EntityProducer<SimpleAssignment> entityProducer;
     private final EntityTopicNameParameters entityTopicNameParameters;
-    public AssigmentEntityProducerService( EntityProducerFactory entityProducerFactory)
+    public AssigmentEntityProducerService(
+            EntityProducerFactory entityProducerFactory,
+
+            EntityTopicService entityTopicService)
     {
         entityProducer = entityProducerFactory.createProducer(SimpleAssignment.class);
         entityTopicNameParameters = EntityTopicNameParameters
                 .builder()
                 .resource("resource-group-membership")
                 .build();
+        entityTopicService.ensureTopic(entityTopicNameParameters, 0);
     }
 
     public void publish(Assignment assignment) {
