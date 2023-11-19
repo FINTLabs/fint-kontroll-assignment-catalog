fff494dd902e87e155af05dd4a8589e8041e3b51
Bernt Andreas Johnsen <berntj@viken.no>
2023-11-19 19:36:01 +0100

    add handling of role memberships WIP

---
+++ src/main/java/no/fintlabs/membership/MembershipConsumerConfiguration.java
@@ -0,0 +1,34 @@
+package no.fintlabs.membership;
+
+import no.fintlabs.kafka.entity.EntityConsumerFactoryService;
+import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import no.fintlabs.role.Role;
+import no.fintlabs.role.RoleService;
+import no.fintlabs.user.User;
+import no.fintlabs.user.UserService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+@Configuration
+public class MembershipConsumerConfiguration {
+    @Bean
+    public ConcurrentMessageListenerContainer<String, Role> membershipConsumer(
+            MembershipService membershipService,
+            EntityConsumerFactoryService entityConsumerFactoryService
+    ){
+        EntityTopicNameParameters entityTopicNameParameters = EntityTopicNameParameters
+                .builder()
+                .resource("role-catalog-membership")
+                .build();
+
+        ConcurrentMessageListenerContainer container = entityConsumerFactoryService.createFactory(
+                        Membership.class,
+                        (ConsumerRecord<String,Membership> consumerRecord)
+                                -> membershipService.save(consumerRecord.value()))
+                .createContainer(entityTopicNameParameters);
+
+        return container;
+    }
+}
