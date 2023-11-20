768f0fbef268822acc8124592f5a8f506aad575a
Bernt Andreas Johnsen <berntj@viken.no>
2023-11-20 08:19:55 +0100

    update to spring 3.1.4

--- src/main/java/no/fintlabs/assignment/AssignmentController.java
+++ src/main/java/no/fintlabs/assignment/AssignmentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.util.Map;
 
 @Slf4j
