30c56e24c6df7727ef215367b6d770b50aea57c3
Bernt Andreas Johnsen <berntj@viken.no>
2023-11-20 16:23:00 +0100

    update membership class

--- src/main/java/no/fintlabs/membership/Membership.java
+++ src/main/java/no/fintlabs/membership/Membership.java
@@ -2,25 +2,22 @@ package no.fintlabs.membership;
 
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Hibernate;
-import org.hibernate.annotations.NaturalId;
 
 import jakarta.persistence.*;
-import java.util.Date;
 import java.util.UUID;
 
 @Getter
 @Setter
 @ToString
-//@RequiredArgsConstructor
 @Slf4j
 @Entity
-@Table(name="Assignmentemberships")
+@Table(name="Assignment_memberships")
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PUBLIC, force=true)
 public class Membership {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Long userId;
+    private String id;
+    private Long roleId;
+    private Long memberId;
+    private UUID identityProviderUserObjectId;
 }
