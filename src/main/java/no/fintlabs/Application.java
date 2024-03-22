7b938ad9b2da55603dbba4b1dda59ca21dc0f719
Morten Solberg <morten@source-code.no>
2024-03-22 07:30:12 +0100

    FKS-737: Add openapi

--- src/main/java/no/fintlabs/Application.java
+++ src/main/java/no/fintlabs/Application.java
@@ -1,9 +1,16 @@
 package no.fintlabs;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import no.fint.antlr.EnableFintFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFintFilter
@@ -16,4 +23,26 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public OpenAPI openAPIDescription() {
+        return new OpenAPI()
+                .info(new Info()
+                              .title("FINT Kontroll assignment catalog")
+                              .version("1.0.0")
+                              .description("API's for assignment of resources to users")
+                              .termsOfService("http://swagger.io/terms/")
+                              .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .components(new Components()
+                                    .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                            .type(SecurityScheme.Type.HTTP)
+                                            .scheme("bearer")
+                                            .bearerFormat("JWT")
+                                            .in(SecurityScheme.In.HEADER)
+                                            .description("Auth key")
+                                            .name("Authorization"))
+                )
+                .addSecurityItem(new SecurityRequirement()
+                                         .addList("bearer-jwt"))
+                ;
+    }
 }
