2c14e4d19b331618e06db2ccf9e6a47ca67ae8eb
Morten Solberg <morten@source-code.no>
2024-05-06 10:40:02 +0200

    FKS-738: Add integrationtest for finding users with assignments that are not deleted.

--- src/main/java/no/fintlabs/user/UserResponseFactory.java
+++ src/main/java/no/fintlabs/user/UserResponseFactory.java
@@ -1,43 +1,13 @@
 package no.fintlabs.user;
 
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
 
-@Component
 public class UserResponseFactory {
-    private final AssigmentUserService assigmentUserService;
-    private final UserService userService;
-    public UserResponseFactory(AssigmentUserService assigmentUserService,
-                               UserService userService
-    ) {
-        this.assigmentUserService = assigmentUserService;
-        this.userService = userService;
-    }
-    public ResponseEntity<Map<String ,Object>> toResponseEntity(
-            Long resourceId,
-            String userType,
-            List<String> orgUnits,
-            List<String> orgUnitsInScope,
-            String searchString,
-            int pageNumber,
-            int pageSize
-    ){
-        UserSpecificationBuilder builder = new UserSpecificationBuilder(resourceId, userType, orgUnits, orgUnitsInScope,searchString);
-
-        Pageable page = PageRequest.of(pageNumber,
-                pageSize,
-                Sort.by("firstName").ascending()
-                        .and(Sort.by("lastName")).ascending());
-
-        Page<AssignmentUser> usersPage = assigmentUserService.findBySearchCriteria(resourceId, builder.build(), page);
-        return toResponseEntity(usersPage);
-    }
-    public ResponseEntity<Map<String, Object>> toResponseEntity(Page<AssignmentUser> assignmentUserPage) {
+    public static ResponseEntity<Map<String, Object>> toResponseEntity(Page<AssignmentUser> assignmentUserPage) {
 
         return new ResponseEntity<>(
                 Map.of( "users", assignmentUserPage.getContent(),
--- src/main/java/no/fintlabs/user/UserSpecificationBuilder.java
+++ src/main/java/no/fintlabs/user/UserSpecificationBuilder.java
@@ -1,14 +1,13 @@
 package no.fintlabs.user;
 
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.assignment.Assignment;
 import no.fintlabs.opa.OpaUtils;
 import no.fintlabs.opa.model.OrgUnitType;
-import no.fintlabs.opa.OpaUtils;
 import org.springframework.data.jpa.domain.Specification;
 
-import jakarta.persistence.criteria.Join;
-import jakarta.persistence.criteria.Root;
 import java.util.List;
 
 @Slf4j
@@ -18,6 +17,7 @@ public class UserSpecificationBuilder {
     private final List<String> orgUnits;
     private final List<String> orgUnitsInScope;
     private final String searchString;
+
     public UserSpecificationBuilder(
             Long resourceId,
             String userType,
@@ -45,11 +45,14 @@ public class UserSpecificationBuilder {
         if (!OpaUtils.isEmptyString(searchString)) {
             spec = spec.and(nameLike(searchString.toLowerCase()));
         }
+
+        spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.isNull(assignmentJoin(root).get("assignmentRemovedDate")));
+
         return spec;
     }
 
     private Specification<User> resourceEquals(Long resourceId) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(resourceJoin(root).get("resourceRef"), resourceId);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(assignmentJoin(root).get("resourceRef"), resourceId);
     }
     private  Specification<User> userTypeEquals(String userType) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(criteriaBuilder.lower(root.get("userType")), userType);
@@ -65,7 +68,7 @@ public class UserSpecificationBuilder {
 
     }
 
-    private Join<User, Assignment> resourceJoin(Root<User> root){
+    private Join<User, Assignment> assignmentJoin(Root<User> root){
         return root.join("assignments");
     }
 
