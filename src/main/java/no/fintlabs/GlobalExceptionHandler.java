541541873ba6fda475d3c68845b32c532d7a16bb
Morten Solberg <morten@source-code.no>
2024-11-18 12:31:56 +0100

    Add global exception handler to skip the /error redirect.

---
+++ src/main/java/no/fintlabs/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package no.fintlabs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        log.error("An unhandled exception occurred with message: {}", ex.getMessage(), ex);
+
+        if (ex instanceof ResponseStatusException) {
+            return ResponseEntity.status(((ResponseStatusException) ex).getStatusCode()).body(ex.getMessage());
+        }
+
+        return ResponseEntity.status(500).body("An unexpected error occurred: " + ex.getMessage());
+    }
+}
