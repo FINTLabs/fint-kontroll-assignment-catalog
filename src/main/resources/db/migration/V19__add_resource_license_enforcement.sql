97df977bc16dd940e0d04f7d14aaafffb3533312
berntjoh <berntj@viken.no>
2024-12-01 19:25:04 +0100

    FKS-1051: adjust flyway

--- src/main/resources/db/migration/V19__add_resource_license_enforcement.sql
+++ src/main/resources/db/migration/V19__add_resource_license_enforcement.sql
@@ -1,5 +1,3 @@
 alter table assignment_resources ADD license_enforcement varchar(255);
-alter table assignments ADD license_enforcement varchar(255);
-alter table flattened_assignments ADD license_enforcement varchar(255);
 alter table assignments ADD resource_consumer_org_unit_id varchar(255);
 alter table flattened_assignments ADD resource_consumer_org_unit_id varchar(255);
