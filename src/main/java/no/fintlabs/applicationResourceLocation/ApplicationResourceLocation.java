40404a039f1a89dac4959a00ceeeb9f3ddd2e775
Erling Jahr <erling.jahr@vigoiks.no>
2025-01-10 10:50:50 +0100

    FKS-1021 added integrationtest. added builder to applicationresourcelocation class

--- src/main/java/no/fintlabs/applicationResourceLocation/ApplicationResourceLocation.java
+++ src/main/java/no/fintlabs/applicationResourceLocation/ApplicationResourceLocation.java
@@ -5,16 +5,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "application_resource_location")
 public class ApplicationResourceLocation{
