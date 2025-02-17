9e16f94c9fc4873c3c92a5cc637616bedcf3c094
Erling Jahr <erling.jahr@vigoiks.no>
2025-02-17 16:05:08 +0100

    FKS-1138 added produser for resourceAvailability

--- src/main/java/no/fintlabs/resource/ResourceAvailability.java
+++ src/main/java/no/fintlabs/resource/ResourceAvailability.java
@@ -1,10 +1,13 @@
 package no.fintlabs.resource;
 
-import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-
+@Getter
+@Setter
 public class ResourceAvailability {
     private Long id;
     private String resourceId;
