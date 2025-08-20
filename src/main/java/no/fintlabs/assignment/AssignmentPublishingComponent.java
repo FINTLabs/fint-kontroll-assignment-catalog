3ae255579d6abff45120ac4a7d048cb996acfa2e
chadaj <agata.chadaj@novari.no>
2025-08-20 10:54:33 +0200

    fix tests

--- src/main/java/no/fintlabs/assignment/AssignmentPublishingComponent.java
+++ src/main/java/no/fintlabs/assignment/AssignmentPublishingComponent.java
@@ -1,23 +1,23 @@
-//package no.fintlabs.assignment;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import no.fintlabs.assignment.flattened.FlattenedAssignmentService;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.concurrent.TimeUnit;
+package no.fintlabs.assignment;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.assignment.flattened.FlattenedAssignmentService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 // TODO check if we need this class
-//@Slf4j
-//@Component
-//public class AssignmentPublishingComponent {
-//    private final FlattenedAssignmentService flattenedAssignmentService;
-//    private final AssigmentEntityProducerService assigmentEntityProducerService;
-//
-//    public AssignmentPublishingComponent(FlattenedAssignmentService flattenedAssignmentService, AssigmentEntityProducerService assigmentEntityProducerService) {
-//        this.flattenedAssignmentService = flattenedAssignmentService;
-//        this.assigmentEntityProducerService = assigmentEntityProducerService;
-//    }
-//
+@Slf4j
+@Component
+public class AssignmentPublishingComponent {
+    private final FlattenedAssignmentService flattenedAssignmentService;
+    private final AssigmentEntityProducerService assigmentEntityProducerService;
+
+    public AssignmentPublishingComponent(FlattenedAssignmentService flattenedAssignmentService, AssigmentEntityProducerService assigmentEntityProducerService) {
+        this.flattenedAssignmentService = flattenedAssignmentService;
+        this.assigmentEntityProducerService = assigmentEntityProducerService;
+    }
+
 //    @Scheduled(
 //            initialDelayString = "${fint.kontroll.assignment-catalog.publishing.initial-delay}",
 //            fixedDelayString = "${fint.kontroll.assignment-catalog.publishing.fixed-delay}",
@@ -41,4 +41,4 @@
 //        flattenedAssignmentService.getFlattenedAssignmentsDeletedNotConfirmed()
 //                .forEach(assigmentEntityProducerService::publishDeletion);
 //    }
-//}
+}
