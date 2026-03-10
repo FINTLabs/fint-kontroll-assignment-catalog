716fb492b5712a00087c0f2a88bb90301388a709
OlaBrustad <ola.brustad@gmail.com>
2026-03-10 11:57:24 +0100

    Revert "baseline-on-migrate"
    This reverts commit 0dd51520abb3da4d993b10a4d24d8704cc5b33da.

--- src/test/java/no/fintlabs/DatabaseIntegrationTest.java
+++ src/test/java/no/fintlabs/DatabaseIntegrationTest.java
@@ -25,6 +25,5 @@ public abstract class DatabaseIntegrationTest {
         registry.add("spring.flyway.user", postgreSQLContainer::getUsername);
         registry.add("spring.flyway.password", postgreSQLContainer::getPassword);
         registry.add("spring.flyway.enabled", () -> "true");
-        registry.add("spring.flyway.baseline-on-migrate", () -> "true");
     }
 }
