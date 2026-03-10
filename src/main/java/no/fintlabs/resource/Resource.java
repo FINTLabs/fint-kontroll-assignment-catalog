912ec1af45134c5d4ffa1b13c915dbee76aea356
OlaBrustad <ola.brustad@gmail.com>
2026-03-10 12:30:16 +0100

    Added missing equals for Resource comparison

--- src/main/java/no/fintlabs/resource/Resource.java
+++ src/main/java/no/fintlabs/resource/Resource.java
@@ -73,13 +73,14 @@ public class Resource extends AuditEntity {
         }
         final Resource resource = (Resource) o;
         return Objects.equals(id, resource.id) && Objects.equals(resourceId, resource.resourceId) && Objects.equals(groupObjectId, resource.groupObjectId) &&
-               Objects.equals(identityProviderGroupObjectId, resource.identityProviderGroupObjectId) && Objects.equals(resourceName, resource.resourceName) &&
+               Objects.equals(identityProviderGroupObjectId, resource.identityProviderGroupObjectId) && Objects.equals(identityProviderGroupName, resource.identityProviderGroupName)
+                && Objects.equals(resourceName, resource.resourceName) &&
                Objects.equals(resourceType, resource.resourceType) && Objects.equals(licenseEnforcement, resource.licenseEnforcement) &&
                 Objects.equals(status, resource.status);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, resourceId, groupObjectId, identityProviderGroupObjectId, resourceName, resourceType, licenseEnforcement, status);
+        return Objects.hash(id, resourceId, groupObjectId, identityProviderGroupObjectId, identityProviderGroupName, resourceName, resourceType, licenseEnforcement, status);
     }
 }
