821a93dbbd5e89253b041c1798266e6e633ae252
berntjoh <berntj@outlook.com>
2026-03-24 14:27:50 +0100

    FKS-1552 add filter with resourceids for getResourcesAssignedToRole

--- src/test/java/no/fintlabs/resource/AssignmentResourceServiceIntegrationTest.java
+++ src/test/java/no/fintlabs/resource/AssignmentResourceServiceIntegrationTest.java
@@ -28,6 +28,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Date;
