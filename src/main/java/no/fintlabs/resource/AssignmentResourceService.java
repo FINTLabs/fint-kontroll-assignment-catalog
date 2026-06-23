f768da095bbb46b50303a2b147ff85688e64e0a5
chadaj <agata.chadaj@novari.no>
2026-06-23 08:46:40 +0200

    merged fixes on deletability of assignemnts

--- src/main/java/no/fintlabs/resource/AssignmentResourceService.java
+++ src/main/java/no/fintlabs/resource/AssignmentResourceService.java
@@ -6,11 +6,8 @@ import no.fintlabs.assignment.AssignmentService;
 import no.fintlabs.assignment.flattened.FlattenedAssignment;
 import no.fintlabs.assignment.flattened.FlattenedAssignmentRepository;
 import no.fintlabs.authorization.AuthorizationUtil;
-import no.fintlabs.resource.ResourceUtil;
-import no.fintlabs.kodeverk.Handhevingstype;
 import no.fintlabs.role.Role;
 import no.fintlabs.user.User;
-import no.fintlabs.user.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -29,20 +26,16 @@ public class AssignmentResourceService {
     private final FlattenedAssignmentRepository flattenedAssignmentRepository;
     private final AuthorizationUtil authorizationUtil;
 
-    private final UserRepository userRepository;
-
     public AssignmentResourceService(
             ResourceRepository resourceRepository,
             AssignmentService assignmentService,
             FlattenedAssignmentRepository flattenedAssignmentRepository,
-            AuthorizationUtil authorizationUtil,
-            UserRepository userRepository
+            AuthorizationUtil authorizationUtil
     ) {
         this.resourceRepository = resourceRepository;
         this.assignmentService = assignmentService;
         this.flattenedAssignmentRepository = flattenedAssignmentRepository;
         this.authorizationUtil = authorizationUtil;
-        this.userRepository = userRepository;
     }
 
     public Page<AssignmentResource> getResourcesAssignedToUser(Long userId, Specification<Resource> spec, Pageable page) {
@@ -99,7 +92,6 @@ public class AssignmentResourceService {
         Assignment assignment = (Assignment) result[4];
         String assignerFirstName = (String) result[5];
         String assignerLastName = (String) result[6];
-        String objectType = (String) result[7];
 
         boolean isDirectAssignment = assignment.isUserAssignment();
         List<String> orgUnitsInScope = authorizationUtil.getAllAuthorizedOrgUnitIDs();
@@ -129,27 +121,4 @@ public class AssignmentResourceService {
 
         return resourceAssignmentUser;
     }
-//
-//    private boolean isDirectAssignment(Assignment assignment) {
-//        return assignment.isUserAssignment();
-//    }
-//    private boolean isDeletableAssignment(Assignment assignment, Resource resource, String objectType) {
-//        List<String> orgUnitsInScope = authorizationUtil.getAllAuthorizedOrgUnitIDs();
-//        return ((objectType.equals("user") && isDirectAssignment(assignment) || objectType.equals("role"))
-//                && (assignment.getApplicationResourceLocationOrgUnitId() != null && orgUnitsInScope.contains(assignment.getApplicationResourceLocationOrgUnitId())
-//                || isResourceUnrestricted(resource)));
-//    }
-//    private boolean isResourceUnrestricted(Resource resource) {
-//        //TODO: temporary solution, should be replaced with a proper check
-//        if (resource.getLicenseEnforcement() == null) {
-//            return false;
-//        }
-//        List<String> unrestrictedEnforcementTypes = List.of(
-//                Handhevingstype.NOTSPECIFIED.name(),
-//                Handhevingstype.NOTSET.name(),
-//                Handhevingstype.FREEALL.name(),
-//                Handhevingstype.FREEEDU.name(),
-//                Handhevingstype.FREESTUDENT.name());
-//        return unrestrictedEnforcementTypes.contains(resource.getLicenseEnforcement());
-//    }
 }
